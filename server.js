a3a943870260b9749b03c88ac752f9d42741f759
samuel <samzyfaps@gmail.com>
2022-10-29 21:07:11 +0100

    new commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3200
+const PORT = process.env.PORT || 3200
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
