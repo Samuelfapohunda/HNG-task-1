e35e84292c99abeb1f6be394e3c90a4dcc16efe1
samuel <samzyfaps@gmail.com>
2022-10-29 19:56:16 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3200
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+
+app.get("/", (req,res) => {
+    res.json({
+      "slackUsername": "Samuel Fapohunda",
+      "Backend": true,
+      "age": 20,
+      "bio": "My name is Samuel Fapohunda. I am 20 years old and a student of the University of Lagos"
+    })
+})
+
+app.get("*", (req,res) => {
+    res.json({
+       msg: 'Page not found'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`);
+});
